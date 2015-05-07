1b4dd8463b148c1daf2f35ae6685e0a76da86bf0
scarviz <scarviz@gmail.com>
2015-05-07 21:17:24 +0900

    add textspeech3,4

--- README.md
+++ README.md
@@ -37,3 +37,49 @@ Serviceのサンプルプログラム。
 読み上げアプリをService化したもの。  
 Activityから読み上げ処理をServiceに移動。  
 バインドしてServiceの読み上げ開始メソッドや停止メソッドを呼べるようにした。  
+
+* TextSpeech3  
+読み上げアプリをService化したもの。  
+TextSpeech2ではActivityがフォアグラウンド状態でないと停止してしまう。  
+Serviceを永続化するにはStartServiceでServiceを起動する必要がある。  
+そのため、Switchを使って、StartService、StopServiceを呼び出すようにした。(MainActivity.java p.58)  
+```java
+// ServiceのStart/Stopを切り替えるスイッチ
+mSwService.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+	@Override
+	public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+		if (isChecked) {
+			StartService();
+		} else {
+			StopService();
+		}
+	}
+});
+```
+ActivityManagerからServiceが起動しているか確認したり、Switchの状態でServiceが起動しているか確認したりしているところもポイント。  
+
+* TextSpeech4  
+読み上げアプリをService化したもの。  
+TextSpeech3に、読み上げ開始時、読み上げ完了時にNotification(通知)を表示するように追加実装した。  
+また、Service停止時には通知を非表示にする。  
+読み上げ開始時、読み上げ完了時に処理するためにリスナーを使っているところがポイント。(TextToSpeechService.java p.93)
+```java
+public void speech(String text) {
+    Log.d(TAG, "speech");
+    stop();
+
+    if (mTts != null) {
+        // 読み上げ開始、完了、エラー時処理用リスナー用のMap
+        // ここで指定した文字列の"utteranceId"が各メソッドの引数に渡る
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "utteranceId");
+
+        // 読み上げる
+        // ※API21以上ならdeprecatedになっている
+        mTts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+        // 読み上げ開始、完了、エラー時処理用リスナーの設定
+        setTtsListener();
+        Log.d(TAG, "start text to speech");
+    }
+}
+```
---
+++ TextSpeech3/app/src/main/java/jp/techinstitute/ti_noda/textspeech/MainActivity.java
@@ -0,0 +1,294 @@
+package jp.techinstitute.ti_noda.textspeech;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+	private static final String TAG = "MainActivity";
+
+	private TextView mTextView;
+	private Switch mSwService;
+	private TextToSpeechService mBoundService;
+	private boolean mIsBound;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mTextView = (TextView) findViewById(R.id.textview);
+		mSwService = (Switch) findViewById(R.id.swService);
+
+		// 開始ボタン押下時処理
+		findViewById(R.id.btnstart).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// サービスが起動状態ならテキストを読み上げる
+				if (activeService()) {
+					speech(mTextView.getText().toString());
+				} else {
+					// サービスを起動する
+					mSwService.setChecked(true);
+				}
+			}
+		});
+		// 停止ボタン押下時処理
+		findViewById(R.id.btnstop).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				stop();
+			}
+		});
+
+		// ServiceのStart/Stopを切り替えるスイッチ
+		mSwService.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+				if (isChecked) {
+					StartService();
+				} else {
+					StopService();
+				}
+			}
+		});
+
+		// 受け取ったIntentから初期化処理を実施
+		init(getIntent());
+
+		// Serviceが起動しているかどうかを確認する
+		boolean isRunning = isRunningService(TextToSpeechService.class.getName());
+		// 起動していればOn、起動していなければOffにする
+		mSwService.setChecked(isRunning);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// サービスが起動状態の場合
+		if (activeService()) {
+			// 接続を確立する
+			doBindService();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		Log.d(TAG, "onPause");
+		super.onPause();
+		// 切断する
+		doUnbindService();
+	}
+
+	@Override
+	public void onDestroy() {
+		Log.d(TAG, "onDestroy");
+		super.onDestroy();
+	}
+
+	/**
+	 * 初期化処理
+	 *
+	 * @param intent
+	 */
+	private void init(Intent intent) {
+		if (intent == null) {
+			Log.d(TAG, "init / intent is null");
+			return;
+		}
+
+		String action = intent.getAction();
+		// テキストが送られてきた場合
+		if (Intent.ACTION_SEND.equals(action)) {
+			// 送られてきたテキストが格納されているもの
+			Bundle extras = intent.getExtras();
+			if (extras == null) {
+				Log.d(TAG, "init / extras is null");
+				return;
+			}
+
+			// テキストを取り出す
+			CharSequence ext = extras.getCharSequence(Intent.EXTRA_TEXT);
+			if (ext != null) {
+				String text = ext.toString();
+				// テキスト表示
+				setText(text);
+			}
+		}
+	}
+
+	/**
+	 * テキスト内容を設定する
+	 *
+	 * @param text
+	 */
+	private void setText(String text) {
+		mTextView.setText(text);
+	}
+
+	/**
+	 * テキストを読み上げる
+	 *
+	 * @param text
+	 */
+	private void speech(String text) {
+		Log.d(TAG, "speech");
+		if (mBoundService != null) {
+			mBoundService.speech(text);
+		}
+	}
+
+	/**
+	 * 停止する
+	 */
+	private void stop() {
+		Log.d(TAG, "stop");
+		if (mBoundService != null) {
+			mBoundService.stop();
+		}
+	}
+
+	/**
+	 * Serviceを開始する
+	 */
+	private void StartService() {
+		Log.d(TAG, "StartService");
+		// Serviceと接続
+		Intent intent = new Intent(this, TextToSpeechService.class);
+		startService(intent);
+		doBindService();
+	}
+
+	/**
+	 * Serviceを停止する
+	 */
+	private void StopService() {
+		Log.d(TAG, "StopService");
+		doUnbindService();
+
+		Intent intent = new Intent(this, TextToSpeechService.class);
+		stopService(intent);
+	}
+
+	/**
+	 * ServiceのBind処理
+	 */
+	private void doBindService() {
+		// Serviceとの接続を確立
+		bindService(new Intent(MainActivity.this, TextToSpeechService.class), mConnection, BIND_AUTO_CREATE);
+		mIsBound = true;
+	}
+
+	/**
+	 * ServiceのUnbind処理
+	 */
+	private void doUnbindService() {
+		if (mIsBound) {
+			// Serviceとの接続を解除
+			unbindService(mConnection);
+			mIsBound = false;
+		}
+	}
+
+	/**
+	 * Serviceと接続するためのコネクション
+	 */
+	private ServiceConnection mConnection = new ServiceConnection() {
+		/**
+		 * Serviceと接続できた場合に呼ばれる
+		 * @param name
+		 * @param service
+		 */
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			Log.d(TAG, "onServiceConnected");
+			// TextToSpeechServiceのインスタンスを取得する
+			mBoundService = ((TextToSpeechService.TextToSpeechServiceIBinder) service).getService();
+		}
+
+		/**
+		 * Serviceとの接続が意図しないタイミングで切断された(異常系)場合に呼ばれる
+		 * @param name
+		 */
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			Log.d(TAG, "onServiceDisconnected");
+			mBoundService = null;
+		}
+	};
+
+	/**
+	 * サービスが起動状態かどうか
+	 *
+	 * @return
+	 */
+	boolean activeService() {
+		return mSwService.isChecked();
+	}
+
+	/**
+	 * サービスが起動中かどうか
+	 *
+	 * @param className
+	 * @return
+	 */
+	boolean isRunningService(String className) {
+		// ActivityManagerのインスタンスを取得する
+		ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+		// 起動中のサービスを取得する
+		List<ActivityManager.RunningServiceInfo> serviceInfos = am.getRunningServices(Integer.MAX_VALUE);
+		// 取得したサービスの数
+		int serviceNum = serviceInfos.size();
+
+		for (int i = 0; i < serviceNum; i++) {
+			// 指定したサービスクラス名(引数のclassName)と一致するものがあった場合、そのサービスが起動中として返す
+			if (serviceInfos.get(i).service.getClassName().equals(className)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	/**
+	 * ********************************************************
+	 * ここから下はメニュー項目関連(今回の講義では使用しない)
+	 * ********************************************************
+	 */
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
